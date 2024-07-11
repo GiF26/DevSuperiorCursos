af07b648a148c4be5f29254500f512f97fd3e2ed
Giovanna Ferreira <giovannapink26@gmail.com>
2024-07-11 08:44:47 -0300

    merge

--- Java_DevSuperior/Student/Object/Student.java
+++ Java_DevSuperior/Student/Object/Student.java
@@ -3,30 +3,36 @@ public class Student {
     public static double nota1;
     public static double nota2;
     public static double nota3;
-    double somaTotalNotasAlunos;
-    double somaTotalTrimestes = 30 + 35 + 35;
-    double notaMinima = somaTotalTrimestes * 60/100;
 
-    public boolean statusAluno() {
-        somaTotalNotasAlunos = nota1 + nota2 +  nota3 ;
+    public double statusAluno() {
+        String status;
+        double somaTotalTrimestes = 30 + 35 + 35;
+        double mediaAluno = (nota1 + nota2 +  nota3 ) / 3;
 
-        if(somaTotalNotasAlunos < notaMinima){
-            return false;
-        }
-        return true;
+//        if(mediaAluno <= mediaAluno * 60 / 100.0){
+//            trataAlunoReprovado()
+//        }
+
+        return mediaAluno;
+
     }
 
     public double trataAlunoReprovado(){
-        return notaMinima - somaTotalNotasAlunos;
+
+
+
+        return 0;
     }
 
     @Override
     public String toString() {
-        if(!statusAluno()){
-            return "Aluno reprovado! Nota final = " + somaTotalNotasAlunos +
-                                    "\nFalta = " + trataAlunoReprovado() + " pontos para a aprovação";
-        }
-        return "Aluno aprovado!" +
-                "\nNota final = " + somaTotalNotasAlunos;
+        return "NOTA FINAL = " + statusAluno();
     }
+
+//    Alex Green
+//        27.00
+//        31.00
+//        32.00
+//    NOTA FINAL = 90.00
+//    PASS
 }
--- Java_DevSuperior/Student/src/Application.java
+++ Java_DevSuperior/Student/src/Application.java
@@ -10,8 +10,27 @@ public class Application {
         obj.nota2 = sc.nextDouble();
         obj.nota3 = sc.nextDouble();
 
-        System.out.println(obj);
-        sc.close();
+        System.out.println(obj.statusAluno());
 
+        if(obj.statusAluno() <= obj.statusAluno() * 60 / 100.0){
+            obj.trataAlunoReprovado();
+        }else{
+            System.out.println("APROVADO");
+        }
+
+        //    Alex Green
+//        27.00
+//        31.00
+//        32.00
+//    NOTA FINAL = 90.00
+//    PASS
+
+//        Alex Green
+//        17.00
+//        20.00
+//        15.00
+//        NOTA FINAL = 52.00
+//        FAILED
+//        FALTARAM 8.00 PONTOS
     }
 }
