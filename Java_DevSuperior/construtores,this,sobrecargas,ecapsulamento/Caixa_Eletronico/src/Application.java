1a9652584aca9f8dd3c18459732e8be9d21e0a5b
Giovanna Ferreira <giovannapink26@gmail.com>
2024-07-12 14:50:55 -0300

    termino de exercicio de fixação : caixa eletronico (correcao)

--- Java_DevSuperior/construtores,this,sobrecargas,ecapsulamento/Caixa_Eletronico/src/Application.java
+++ Java_DevSuperior/construtores,this,sobrecargas,ecapsulamento/Caixa_Eletronico/src/Application.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
 public class Application {
+    public static Conta conta1 ;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Conta conta1 ;
 
         System.out.println("Preencha os dados para a criacao da conta1");
         System.out.println("Nome do titular");
@@ -12,27 +13,33 @@ public class Application {
         System.out.println("Deseja fazer deposito inicial? Pressione 1 para nao e 2 para sim");
         int decisao = sc.nextInt();
 
-            switch (decisao){
-                case 1:
+            switch(decisao){
+                case 1 :
+                    conta1 = new Conta(nomeTitular,26003005);
+                    System.out.println("\nSua conta foi criada! seus dados sao : " +
+                            "\nTitular: " + conta1.getNomeTitular() +
+                            "\nNumero da conta: " + conta1.getNroConta());
+
+                    break;
+
+                case 2:
                     System.out.println("De quanto gostaria de depositar ?");
                     conta1.deposito(sc.nextDouble());
 
-                    conta1 = new Conta(conta1.getNomeTitular(),26003005);
-                    System.out.println("Sua conta1 foi criada e seus dados sao : " +
-                            "\nTitular: " + conta1.getNomeTitular() +
-                            "\nNumero da conta1: " + conta1.getNroConta());
-                    break;
-                case 2:
-                    conta1 = new Conta(conta1.getNomeTitular(),26003005, conta1.getSaldo() );
-                    System.out.println("Sua conta1 foi criada e seus dados sao : \n" +
+                    conta1 = new Conta(nomeTitular, 26003005, conta1.getSaldo());
+                    System.out.println("Sua conta foi criada e seus dados sao : \n" +
                             "\nTitular: " + conta1.getNomeTitular() +
                             "\nNumero da conta1: " + conta1.getNroConta() +
-                            "\nDeposito inicial: + R$" + conta1.getSaldo()) ;
+                            "\nDeposito inicial: + R$" + conta1.getSaldo());
                     break;
 
+                default:
+                    System.out.println("Conta nao criada , operação invalida");
+                    break;
             }
 
         while (decisao != 0){
+
             System.out.println("\nDigite:" +
                     " \n1 para realizar um saque;" +
                     " \n2 para realizar um deposito;" +
@@ -41,24 +48,25 @@ public class Application {
 
             switch (decisao){
                 case 1:
-
                     conta1.saque(sc.nextDouble());
                     System.out.println("Titular: " + conta1.getNomeTitular() +
-                            "\nNumero da conta1: " + conta1.getNroConta() +
-                            "\nSaldo Atual: - R$" + conta1.getSaldo());
+                            ",Numero da Conta: " + conta1.getNroConta() +
+                            ",Saldo Atual: - R$" + conta1.getSaldo());
                     break;
                 case 2:
                     conta1.deposito(sc.nextDouble());
                     System.out.println("Titular: " + conta1.getNomeTitular() +
-                            "\nNumero da conta1: " + conta1.getNroConta() +
-                            "\nSaldo Atual: + R$" + conta1.getSaldo());
+                            ",Numero da Conta: " + conta1.getNroConta() +
+                            ",Saldo Atual: + R$" + conta1.getSaldo());
                     break;
                 default:
-                    System.out.println("Voce saiu da conta1!");
+                    System.out.println("Voce saiu da conta!");
                     decisao = 0;
                     break;
             }
         }
-
     }
 }
+
+
+
