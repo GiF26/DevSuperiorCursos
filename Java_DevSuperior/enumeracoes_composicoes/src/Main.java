e31aa938a4d7f6dd6548b192c5846a7c0a624a7d
Giovanna Ferreira <83253307+GiF26@users.noreply.github.com>
2024-09-28 18:27:03 -0300

    Lista de exercicios matrizes

--- Java_DevSuperior/enumeracoes_composicoes/src/Main.java
+++ Java_DevSuperior/enumeracoes_composicoes/src/Main.java
@@ -4,12 +4,7 @@ import Enums.WorkerLevel;
 import Objects.Departament;
 import Objects.HourContract;
 import Objects.Worker;
-import com.sun.org.apache.xerces.internal.util.SAX2XNI;
-import jdk.internal.cmm.SystemResourcePressureImpl;
-
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -17,38 +12,51 @@ public class Main {
     static ArrayList<HourContract> contracts = new ArrayList<>();
     static Worker wk = new Worker();
     static Departament dp;
+    static Scanner sc = new Scanner(System.in);
+    static int qtyContract = 0;
+
 
     public static void main(String[] args) {
-        try(Scanner sc = new Scanner(System.in)){
-            addDepartment();
-            addWorker();
-            addContract();
-            calcIncome();
-        }
+        addDepartment();
+        addWorker();
+        addContract(qtyContract);
+        calcIncome();
+        sc.close();
     }
 
     public static void addDepartment(){
-        Scanner sc = new Scanner(System.in);
-        dp = new Departament(sc.nextLine());
         System.out.println("Enter departament's name: ");
-        sc.close();
+        dp = new Departament(sc.next());
     }
 
     public static void addWorker(){
-        Scanner sc = new Scanner(System.in);
         System.out.println("Enter worker data:");
+
+        sc.nextLine();
         System.out.println("Name");
         wk.setName(sc.nextLine());
+
         System.out.print("Level");
-        String level = sc.next().toUpperCase();
-        wk.setLevel(WorkerLevel.valueOf(level));
+        String level = sc.nextLine().toUpperCase();
+        switch (level){
+            case "JUNIOR":
+                wk.setLevel(WorkerLevel.JUNIOR);
+                break;
+            case "MID LEVEL":
+                wk.setLevel(WorkerLevel.MID_LEVEL);
+                break;
+            case "SENIOR":
+                wk.setLevel(WorkerLevel.SENIOR);
+                break;
+        }
+
+
         System.out.println("Base salary");
-        wk.setBaseSalary(sc.nextDouble());
-        sc.close();
+        double baseSalary = sc.nextDouble();
+        wk.setBaseSalary(baseSalary);
     }
 
-    public static void addContract(){
-        Scanner sc = new Scanner(System.in);
+    public static void addContract(int qtyContract){
         System.out.println("How many contracts to this worker");
         int qtyContractWorker = sc.nextInt();
 
@@ -58,9 +66,10 @@ public class Main {
             System.out.println("Enter #" + contador + "data:" );
 
             System.out.println("Date (DD/MM/YYYY):");
-//            String parseDate = String.format(sc.next(), "DD/MM/YYYY");
+            String parseDate = String.format(sc.next(), "DD/MM/YYYY");
+            System.out.println(parseDate);
 //            Date date = Date.parse(parseDate);
-//            hc.setDate(Date.parse(date));
+            //hc.setDate(Date.parse(date));
 
             System.out.println("Value per hour");
             hc.setValuePreHour(sc.nextDouble());
@@ -69,12 +78,9 @@ public class Main {
 
             contracts.add(hc);
         }
-        sc.close();
     }
 
     public static void calcIncome(){
-        Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter month and year to calculate income (MM/YYYY):");
         String income = sc.next();
 
@@ -87,8 +93,6 @@ public class Main {
         System.out.println(incomeValue);
 
         contracts.clear();
-
-        sc.close();
     }
 
 
