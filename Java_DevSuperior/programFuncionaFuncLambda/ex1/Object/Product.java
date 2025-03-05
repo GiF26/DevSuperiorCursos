477c350b458c7e85bd7646f7203a0f94fc3537ab
Giovana Ferreira <giovannapink26@gmail.com>
2025-03-05 18:41:10 -0300

    Desafio: Analise de vendas parte 1 - correcao de critica

---
+++ Java_DevSuperior/programFuncionaFuncLambda/ex1/Object/Product.java
@@ -0,0 +1,27 @@
+public class Product {
+    private String descricao;
+    private double valor;
+
+    public Product(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+
+}
---
+++ Java_DevSuperior/programFuncionaFuncLambda/ex2/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+//Objetivo:
+//Escreva um programa para ler um conjunto de dados de funcionários a partir de um arquivo em formato .csv. O arquivo contém
+//informações como nome, cargo e salário. O programa deve calcular o salário médio dos funcionários e, em seguida, exibir os nomes
+//dos funcionários cujo salário é inferior ao salário médio, ordenados alfabeticamente.
